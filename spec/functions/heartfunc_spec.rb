9a91e88a92a5b82a1b3b9a20899db7846c247c1d
simonmh2u <simonmh2u@gmail.com>
2014-07-16 15:20:36 +0530

    Remove incorrect template calls from functions

--- spec/functions/heartfunc_spec.rb
+++ spec/functions/heartfunc_spec.rb
@@ -8,18 +8,6 @@ require 'spec_helper'
 	it { should run.with_params('eth0') }
 	end
 	
-	describe 'template' do
-	it { should run.with_params('heartbeat/authkeys') }
-	end
-	
-	describe 'template' do
-	it { should run.with_params('heartbeat/haresources') }
-	end
-	
-	describe 'template' do
-	it { should run.with_params('heartbeat/ha.cf') }
-	end
-	
 #	describe 'mask2cidr' do
 #	it { should run.with_params('eth0') }
 #	end
